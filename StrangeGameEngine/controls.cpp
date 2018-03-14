1288f64fac67cd7ae8e9fbda8c0f07930b8f626c
Lakstoties <bryanws@gmail.com>
2018-03-14 10:35:21 -0500

    Created direct mapping of External SGE Keymap to Internal GLFW keymap

--- StrangeGameEngine/controls.cpp
+++ StrangeGameEngine/controls.cpp
@@ -64,7 +64,162 @@ namespace SGE
 			{
 				std::memcpy(targetKeyboardStatusArray, Status, sizeof(Status));
 			}
+
+			//
+			//  Function to convert a keyboard status to ASCII characters
+			//
+			int StatusToASCII(bool targetKeyboardStatusArray[NUMBER_OF_KEYS], char* targetASCIIArray, int maxASCIICharacters)
+			{
+				//
+				//  Scan through array for keys that map to ASCII characters
+				//
+				
+				//  Check letter keys
+			
+
+				//  Check nubmer keys
+				
+				return 0;
+			}
+
 		}
+
+		//
+		//  Keymappings from External SGE to Internal GLFW
+		//
+		namespace Keymap
+		{
+			//
+			//  Printable keys
+			//
+			const int KEY_SPACE = GLFW_KEY_SPACE;			//' ' 
+			const int KEY_APOSTROPHE = GLFW_KEY_APOSTROPHE;	// ' 
+			const int KEY_COMMA = GLFW_KEY_COMMA;			// ,
+			const int KEY_MINUS = GLFW_KEY_MINUS;			// - 
+			const int KEY_PERIOD = GLFW_KEY_PERIOD;			// . 
+			const int KEY_SLASH = GLFW_KEY_SLASH;			// / 
+			const int KEY_0 = GLFW_KEY_0;					// 0
+			const int KEY_1 = GLFW_KEY_1;					// 1
+			const int KEY_2 = GLFW_KEY_2;					// 2
+			const int KEY_3 = GLFW_KEY_3;					// 3
+			const int KEY_4 = GLFW_KEY_4;					// 4
+			const int KEY_5 = GLFW_KEY_5;					// 5
+			const int KEY_6 = GLFW_KEY_6;					// 6
+			const int KEY_7 = GLFW_KEY_7;					// 7
+			const int KEY_8 = GLFW_KEY_8;					// 8
+			const int KEY_9 = GLFW_KEY_9;					// 9
+			const int KEY_SEMICOLON = GLFW_KEY_SEMICOLON;	// ; 
+			const int KEY_EQUAL = GLFW_KEY_EQUAL;			// = 
+			const int KEY_A = GLFW_KEY_A;					// a/A
+			const int KEY_B = GLFW_KEY_B;					// b/B
+			const int KEY_C = GLFW_KEY_C;					// c/C
+			const int KEY_D = GLFW_KEY_D;					// d/D
+			const int KEY_E = GLFW_KEY_E;					// e/E
+			const int KEY_F = GLFW_KEY_F;					// f/F
+			const int KEY_G = GLFW_KEY_G;					// g/G
+			const int KEY_H = GLFW_KEY_H;					// h/H
+			const int KEY_I = GLFW_KEY_I;					// i/I
+			const int KEY_J = GLFW_KEY_J;					// j/J
+			const int KEY_K = GLFW_KEY_K;					// k/K
+			const int KEY_L = GLFW_KEY_L;					// l/L
+			const int KEY_M = GLFW_KEY_M;					// m/M
+			const int KEY_N = GLFW_KEY_N;					// n/N
+			const int KEY_O = GLFW_KEY_O;					// o/O
+			const int KEY_P = GLFW_KEY_P;					// p/P
+			const int KEY_Q = GLFW_KEY_Q;					// q/Q
+			const int KEY_R = GLFW_KEY_R;					// r/R
+			const int KEY_S = GLFW_KEY_S;					// s/S
+			const int KEY_T = GLFW_KEY_T;					// t/T
+			const int KEY_U = GLFW_KEY_U;					// u/U
+			const int KEY_V = GLFW_KEY_V;					// v/V
+			const int KEY_W = GLFW_KEY_W;					// w/W
+			const int KEY_X = GLFW_KEY_X;					// x/X
+			const int KEY_Y = GLFW_KEY_Y;					// y/Y
+			const int KEY_Z = GLFW_KEY_Z;					// z/Z
+			const int KEY_LEFT_BRACKET = GLFW_KEY_LEFT_BRACKET;	// [ 
+			const int KEY_BACKSLASH = GLFW_KEY_BACKSLASH;		// "\" 
+			const int KEY_RIGHT_BRACKET = GLFW_KEY_RIGHT_BRACKET;	// ] 
+			const int KEY_GRAVE_ACCENT = GLFW_KEY_GRAVE_ACCENT;	// `
+			const int KEY_WORLD_1 = GLFW_KEY_WORLD_1;		// non-US #1
+			const int KEY_WORLD_2 = GLFW_KEY_WORLD_2;		// non-US #2
+
+			//
+			// Function keys
+			//
+			const int KEY_ESCAPE = GLFW_KEY_ESCAPE;				// Esc
+			const int KEY_ENTER = GLFW_KEY_ENTER;				// Enter
+			const int KEY_TAB = GLFW_KEY_TAB;					// Tab
+			const int KEY_BACKSPACE = GLFW_KEY_BACKSPACE;		// Backspace
+			const int KEY_INSERT = GLFW_KEY_INSERT;				// Insert
+			const int KEY_DELETE = GLFW_KEY_DELETE;				// Delete
+			const int KEY_RIGHT = GLFW_KEY_RIGHT;				// ->
+			const int KEY_LEFT = GLFW_KEY_LEFT;					// <-
+			const int KEY_DOWN = GLFW_KEY_DOWN;					// v
+			const int KEY_UP = GLFW_KEY_UP;						// ^
+			const int KEY_PAGE_UP = GLFW_KEY_PAGE_UP;			// Page Up
+			const int KEY_PAGE_DOWN = GLFW_KEY_PAGE_DOWN;		// Page Down
+			const int KEY_HOME = GLFW_KEY_HOME;					// Home
+			const int KEY_END = GLFW_KEY_END;					// End
+			const int KEY_CAPS_LOCK = GLFW_KEY_CAPS_LOCK;		// Caps Lock
+			const int KEY_SCROLL_LOCK = GLFW_KEY_SCROLL_LOCK;	// Scroll Lock
+			const int KEY_NUM_LOCK = GLFW_KEY_NUM_LOCK;			// Num Lock
+			const int KEY_PRINT_SCREEN = GLFW_KEY_PRINT_SCREEN;	// Print Screen
+			const int KEY_PAUSE = GLFW_KEY_PAUSE;				// Pause
+			const int KEY_F1 = GLFW_KEY_F1;						// F1
+			const int KEY_F2 = GLFW_KEY_F2;						// F2
+			const int KEY_F3 = GLFW_KEY_F3;						// F3
+			const int KEY_F4 = GLFW_KEY_F4;						// F4
+			const int KEY_F5 = GLFW_KEY_F5;						// F5
+			const int KEY_F6 = GLFW_KEY_F6;						// F6
+			const int KEY_F7 = GLFW_KEY_F7;						// F7
+			const int KEY_F8 = GLFW_KEY_F8;						// F8
+			const int KEY_F9 = GLFW_KEY_F9;						// F9
+			const int KEY_F10 = GLFW_KEY_F10;					// F10
+			const int KEY_F11 = GLFW_KEY_F11;					// F11
+			const int KEY_F12 = GLFW_KEY_F12;					// F12
+			const int KEY_F13 = GLFW_KEY_F13;					// F13
+			const int KEY_F14 = GLFW_KEY_F14;					// F14
+			const int KEY_F15 = GLFW_KEY_F15;					// F15
+			const int KEY_F16 = GLFW_KEY_F16;					// F16
+			const int KEY_F17 = GLFW_KEY_F17;					// F17
+			const int KEY_F18 = GLFW_KEY_F18;					// F18
+			const int KEY_F19 = GLFW_KEY_F19;					// F19
+			const int KEY_F20 = GLFW_KEY_F20;					// F20
+			const int KEY_F21 = GLFW_KEY_F21;					// F21
+			const int KEY_F22 = GLFW_KEY_F22;					// F22
+			const int KEY_F23 = GLFW_KEY_F23;					// F23
+			const int KEY_F24 = GLFW_KEY_F24;					// F24
+			const int KEY_F25 = GLFW_KEY_F25;					// F25
+			const int KEY_KP_0 = GLFW_KEY_KP_0;					// Keypad 0
+			const int KEY_KP_1 = GLFW_KEY_KP_1;					// Keypad 1
+			const int KEY_KP_2 = GLFW_KEY_KP_2;					// Keypad 2
+			const int KEY_KP_3 = GLFW_KEY_KP_3;					// Keypad 3
+			const int KEY_KP_4 = GLFW_KEY_KP_4;					// Keypad 4
+			const int KEY_KP_5 = GLFW_KEY_KP_5;					// Keypad 5
+			const int KEY_KP_6 = GLFW_KEY_KP_6;					// Keypad 6
+			const int KEY_KP_7 = GLFW_KEY_KP_7;					// Keypad 7
+			const int KEY_KP_8 = GLFW_KEY_KP_8;					// Keypad 8
+			const int KEY_KP_9 = GLFW_KEY_KP_9;					// Keypad 9
+			const int KEY_KP_DECIMAL = GLFW_KEY_KP_DECIMAL;		// Keypad .
+			const int KEY_KP_DIVIDE = GLFW_KEY_KP_DIVIDE;		// Keypad /
+			const int KEY_KP_MULTIPLY = GLFW_KEY_KP_MULTIPLY;	// Keypad *
+			const int KEY_KP_SUBTRACT = GLFW_KEY_KP_SUBTRACT;	// Keypad -
+			const int KEY_KP_ADD = GLFW_KEY_KP_ADD;				// Keypad +
+			const int KEY_KP_ENTER = GLFW_KEY_KP_ENTER;			// Keypad Enter
+			const int KEY_KP_EQUAL = GLFW_KEY_KP_EQUAL;			// Keypad =
+			const int KEY_LEFT_SHIFT = GLFW_KEY_LEFT_SHIFT;		// Left Shift
+			const int KEY_LEFT_CONTROL = GLFW_KEY_LEFT_CONTROL;	// Left Control
+			const int KEY_LEFT_ALT = GLFW_KEY_LEFT_ALT;			// Left Alt
+			const int KEY_LEFT_SUPER = GLFW_KEY_LEFT_SUPER;		// Left Super/Windows
+			const int KEY_RIGHT_SHIFT = GLFW_KEY_RIGHT_SHIFT;	// Right Shift	
+			const int KEY_RIGHT_CONTROL = GLFW_KEY_RIGHT_CONTROL;	// Right Control
+			const int KEY_RIGHT_ALT = GLFW_KEY_RIGHT_ALT;		// Right Alt
+			const int KEY_RIGHT_SUPER = GLFW_KEY_RIGHT_SUPER;	// Right Super/Windows
+			const int KEY_MENU = GLFW_KEY_MENU;					// Menu
+
+			const int KEY_LAST = KEY_MENU;						// Last
+		}
+
 
 		//
 		//  Flag to indicate to the handle events thread to continue or not
--- StrangeGameEngine/include/SGE/controls.h
+++ StrangeGameEngine/include/SGE/controls.h
@@ -62,6 +62,11 @@ namespace SGE
 			//  Function to save the current keyboard status array to another array
 			//
 			void SaveStatus(bool targetKeyboardStatusArray[NUMBER_OF_KEYS]);
+
+			//
+			//  Function to convert a keyboard status to ASCII characters
+			//
+			int StatusToASCII(bool targetKeyboardStatusArray[NUMBER_OF_KEYS], char* targetASCIIArray, int maxASCIICharacters);
 		}
 
 		//
@@ -81,130 +86,130 @@ namespace SGE
 		namespace Keymap
 		{
 			//Printable keys
-			const int KEY_SPACE =		32;		//' ' 
-			const int KEY_APOSTROPHE =	39;		// ' 
-			const int KEY_COMMA =		44;		// ,
-			const int KEY_MINUS =		45;		// - 
-			const int KEY_PERIOD =		46;		// . 
-			const int KEY_SLASH =		47;		// / 
-			const int KEY_0 =			48;		// 0
-			const int KEY_1 =			49;		// 1
-			const int KEY_2 =           50;		// 2
-			const int KEY_3 =           51;		// 3
-			const int KEY_4 =           52;		// 4
-			const int KEY_5 =           53;		// 5
-			const int KEY_6 =			54;		// 6
-			const int KEY_7 =			55;		// 7
-			const int KEY_8 =			56;		// 8
-			const int KEY_9 =			57;		// 9
-			const int KEY_SEMICOLON =	59;		// ; 
-			const int KEY_EQUAL =		61;		// = 
-			const int KEY_A =			65;		// a/A
-			const int KEY_B =			66;		// b/B
-			const int KEY_C =			67;		// c/C
-			const int KEY_D =			68;		// d/D
-			const int KEY_E =			69;		// e/E
-			const int KEY_F =			70;		// f/F
-			const int KEY_G =			71;		// g/G
-			const int KEY_H =			72;		// h/H
-			const int KEY_I =			73;		// i/I
-			const int KEY_J =			74;		// j/J
-			const int KEY_K =			75;		// k/K
-			const int KEY_L =			76;		// l/L
-			const int KEY_M =			77;		// m/M
-			const int KEY_N =			78;		// n/N
-			const int KEY_O =			79;		// o/O
-			const int KEY_P =			80;		// p/P
-			const int KEY_Q =			81;		// q/Q
-			const int KEY_R =			82;		// r/R
-			const int KEY_S =			83;		// s/S
-			const int KEY_T =			84;		// t/T
-			const int KEY_U =			85;		// u/U
-			const int KEY_V =			86;		// v/V
-			const int KEY_W =			87;		// w/W
-			const int KEY_X =			88;		// x/X
-			const int KEY_Y =			89;		// y/Y
-			const int KEY_Z =			90;		// z/Z
-			const int KEY_LEFT_BRACKET = 91;	// [ 
-			const int KEY_BACKSLASH =	92;		// "\" 
-			const int KEY_RIGHT_BRACKET = 93;	// ] 
-			const int KEY_GRAVE_ACCENT = 96;	// `
-			const int KEY_WORLD_1 =		161;	// non-US #1
-			const int KEY_WORLD_2 =		162;	// non-US #2
+			extern const int KEY_SPACE;			//' ' 
+			extern const int KEY_APOSTROPHE;	// ' 
+			extern const int KEY_COMMA;			// ,
+			extern const int KEY_MINUS;			// - 
+			extern const int KEY_PERIOD;		// . 
+			extern const int KEY_SLASH;			// / 
+			extern const int KEY_0;				// 0
+			extern const int KEY_1;				// 1
+			extern const int KEY_2;				// 2
+			extern const int KEY_3;				// 3
+			extern const int KEY_4;				// 4
+			extern const int KEY_5;				// 5
+			extern const int KEY_6;				// 6
+			extern const int KEY_7;				// 7
+			extern const int KEY_8;				// 8
+			extern const int KEY_9;				// 9
+			extern const int KEY_SEMICOLON;		// ; 
+			extern const int KEY_EQUAL;			// = 
+			extern const int KEY_A;				// a/A
+			extern const int KEY_B;				// b/B
+			extern const int KEY_C;				// c/C
+			extern const int KEY_D;				// d/D
+			extern const int KEY_E;				// e/E
+			extern const int KEY_F;				// f/F
+			extern const int KEY_G;				// g/G
+			extern const int KEY_H;				// h/H
+			extern const int KEY_I;				// i/I
+			extern const int KEY_J;				// j/J
+			extern const int KEY_K;				// k/K
+			extern const int KEY_L;				// l/L
+			extern const int KEY_M;				// m/M
+			extern const int KEY_N;				// n/N
+			extern const int KEY_O;				// o/O
+			extern const int KEY_P;				// p/P
+			extern const int KEY_Q;				// q/Q
+			extern const int KEY_R;				// r/R
+			extern const int KEY_S;				// s/S
+			extern const int KEY_T;				// t/T
+			extern const int KEY_U;				// u/U
+			extern const int KEY_V;				// v/V
+			extern const int KEY_W;				// w/W
+			extern const int KEY_X;				// x/X
+			extern const int KEY_Y;				// y/Y
+			extern const int KEY_Z;				// z/Z
+			extern const int KEY_LEFT_BRACKET;	// [ 
+			extern const int KEY_BACKSLASH;		// "\" 
+			extern const int KEY_RIGHT_BRACKET;	// ] 
+			extern const int KEY_GRAVE_ACCENT;	// `
+			extern const int KEY_WORLD_1;		// non-US #1
+			extern const int KEY_WORLD_2;		// non-US #2
 
 			// Function keys
-			const int KEY_ESCAPE =		256;	// Esc
-			const int KEY_ENTER =		257;	// Enter
-			const int KEY_TAB =			258;	// Tab
-			const int KEY_BACKSPACE =	259;	// Backspace
-			const int KEY_INSERT =		260;	// Insert
-			const int KEY_DELETE =		261;	// Delete
-			const int KEY_RIGHT =		262;	// ->
-			const int KEY_LEFT =		263;	// <-
-			const int KEY_DOWN =		264;	// v
-			const int KEY_UP =			265;	// ^
-			const int KEY_PAGE_UP =		266;	// Page Up
-			const int KEY_PAGE_DOWN =	267;	// Page Down
-			const int KEY_HOME =		268;	// Home
-			const int KEY_END =			269;	// End
-			const int KEY_CAPS_LOCK =	280;	// Caps Lock
-			const int KEY_SCROLL_LOCK = 281;	// Scroll Lock
-			const int KEY_NUM_LOCK =	282;	// Num Lock
-			const int KEY_PRINT_SCREEN = 283;	// Print Screen
-			const int KEY_PAUSE =		284;	// Pause
-			const int KEY_F1 =			290;	// F1
-			const int KEY_F2 =			291;	// F2
-			const int KEY_F3 =			292;	// F3
-			const int KEY_F4 =			293;	// F4
-			const int KEY_F5 =			294;	// F5
-			const int KEY_F6 =			295;	// F6
-			const int KEY_F7 =			296;	// F7
-			const int KEY_F8 =			297;	// F8
-			const int KEY_F9 =			298;	// F9
-			const int KEY_F10 =			299;	// F10
-			const int KEY_F11 =			300;	// F11
-			const int KEY_F12 =			301;	// F12
-			const int KEY_F13 =			302;	// F13
-			const int KEY_F14 =			303;	// F14
-			const int KEY_F15 =			304;	// F15
-			const int KEY_F16 =			305;	// F16
-			const int KEY_F17 =			306;	// F17
-			const int KEY_F18 =			307;	// F18
-			const int KEY_F19 =			308;	// F19
-			const int KEY_F20 =			309;	// F20
-			const int KEY_F21 =			310;	// F21
-			const int KEY_F22 =			311;	// F22
-			const int KEY_F23 =			312;	// F23
-			const int KEY_F24 =			313;	// F24
-			const int KEY_F25 =			314;	// F25
-			const int KEY_KP_0 =		320;	// Keypad 0
-			const int KEY_KP_1 =		321;	// Keypad 1
-			const int KEY_KP_2 =		322;	// Keypad 2
-			const int KEY_KP_3 =		323;	// Keypad 3
-			const int KEY_KP_4 =		324;	// Keypad 4
-			const int KEY_KP_5 =		325;	// Keypad 5
-			const int KEY_KP_6 =		326;	// Keypad 6
-			const int KEY_KP_7 =		327;	// Keypad 7
-			const int KEY_KP_8 =		328;	// Keypad 8
-			const int KEY_KP_9 =		329;	// Keypad 9
-			const int KEY_KP_DECIMAL =	330;	// Keypad .
-			const int KEY_KP_DIVIDE =	331;	// Keypad /
-			const int KEY_KP_MULTIPLY = 332;	// Keypad *
-			const int KEY_KP_SUBTRACT = 333;	// Keypad -
-			const int KEY_KP_ADD =		334;	// Keypad +
-			const int KEY_KP_ENTER =	335;	// Keypad Enter
-			const int KEY_KP_EQUAL =	336;	// Keypad =
-			const int KEY_LEFT_SHIFT =	340;	// Left Shift
-			const int KEY_LEFT_CONTROL = 341;	// Left Control
-			const int KEY_LEFT_ALT =	342;	// Left Alt
-			const int KEY_LEFT_SUPER =	343;	// Left Super/Windows
-			const int KEY_RIGHT_SHIFT = 344;	// Right Shift	
-			const int KEY_RIGHT_CONTROL = 345;	// Right Control
-			const int KEY_RIGHT_ALT = 346;		// Right Alt
-			const int KEY_RIGHT_SUPER = 347;	// Right Super/Windows
-			const int KEY_MENU = 348;			// Menu
+			extern const int KEY_ESCAPE;		// Esc
+			extern const int KEY_ENTER;			// Enter
+			extern const int KEY_TAB;			// Tab
+			extern const int KEY_BACKSPACE;		// Backspace
+			extern const int KEY_INSERT;		// Insert
+			extern const int KEY_DELETE;		// Delete
+			extern const int KEY_RIGHT;			// ->
+			extern const int KEY_LEFT;			// <-
+			extern const int KEY_DOWN;			// v
+			extern const int KEY_UP;			// ^
+			extern const int KEY_PAGE_UP;		// Page Up
+			extern const int KEY_PAGE_DOWN;		// Page Down
+			extern const int KEY_HOME;			// Home
+			extern const int KEY_END;			// End
+			extern const int KEY_CAPS_LOCK;		// Caps Lock
+			extern const int KEY_SCROLL_LOCK;	// Scroll Lock
+			extern const int KEY_NUM_LOCK;		// Num Lock
+			extern const int KEY_PRINT_SCREEN;	// Print Screen
+			extern const int KEY_PAUSE;			// Pause
+			extern const int KEY_F1;			// F1
+			extern const int KEY_F2;			// F2
+			extern const int KEY_F3;			// F3
+			extern const int KEY_F4;			// F4
+			extern const int KEY_F5;			// F5
+			extern const int KEY_F6;			// F6
+			extern const int KEY_F7;			// F7
+			extern const int KEY_F8;			// F8
+			extern const int KEY_F9;			// F9
+			extern const int KEY_F10;			// F10
+			extern const int KEY_F11;			// F11
+			extern const int KEY_F12;			// F12
+			extern const int KEY_F13;			// F13
+			extern const int KEY_F14;			// F14
+			extern const int KEY_F15;			// F15
+			extern const int KEY_F16;			// F16
+			extern const int KEY_F17;			// F17
+			extern const int KEY_F18;			// F18
+			extern const int KEY_F19;			// F19
+			extern const int KEY_F20;			// F20
+			extern const int KEY_F21;			// F21
+			extern const int KEY_F22;			// F22
+			extern const int KEY_F23;			// F23
+			extern const int KEY_F24;			// F24
+			extern const int KEY_F25;			// F25
+			extern const int KEY_KP_0;			// Keypad 0
+			extern const int KEY_KP_1;			// Keypad 1
+			extern const int KEY_KP_2;			// Keypad 2
+			extern const int KEY_KP_3;			// Keypad 3
+			extern const int KEY_KP_4;			// Keypad 4
+			extern const int KEY_KP_5;			// Keypad 5
+			extern const int KEY_KP_6;			// Keypad 6
+			extern const int KEY_KP_7;			// Keypad 7
+			extern const int KEY_KP_8;			// Keypad 8
+			extern const int KEY_KP_9;			// Keypad 9
+			extern const int KEY_KP_DECIMAL;	// Keypad .
+			extern const int KEY_KP_DIVIDE;		// Keypad /
+			extern const int KEY_KP_MULTIPLY;	// Keypad *
+			extern const int KEY_KP_SUBTRACT;	// Keypad -
+			extern const int KEY_KP_ADD;		// Keypad +
+			extern const int KEY_KP_ENTER;		// Keypad Enter
+			extern const int KEY_KP_EQUAL;		// Keypad =
+			extern const int KEY_LEFT_SHIFT;	// Left Shift
+			extern const int KEY_LEFT_CONTROL;	// Left Control
+			extern const int KEY_LEFT_ALT;		// Left Alt
+			extern const int KEY_LEFT_SUPER;	// Left Super/Windows
+			extern const int KEY_RIGHT_SHIFT;	// Right Shift	
+			extern const int KEY_RIGHT_CONTROL;	// Right Control
+			extern const int KEY_RIGHT_ALT;		// Right Alt
+			extern const int KEY_RIGHT_SUPER;	// Right Super/Windows
+			extern const int KEY_MENU;			// Menu
 
-			const int KEY_LAST = KEY_MENU;		// Last
+			extern const int KEY_LAST;			// Last
 		}
 	}
 }
